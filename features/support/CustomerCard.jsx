f03b0c500f9de97d7b90a5806f04d7fe8a323c60
shyun <exriik@gmail.com>
2025-12-19 11:58:46 +0900

    default 추가

--- features/support/CustomerCard.jsx
+++ features/support/CustomerCard.jsx
@@ -1,14 +1,25 @@
+// "use client";
+
+// // src/features/customer/components/CustomerCard.jsx
+// import React from "react";
+
+// export function CustomerCard({ icon, title, children }) {
+//   const Icon = icon;
+
+//   return (
+//     <div className="service-card">
+//       <Icon className="icon" />
+//       <h3>{title}</h3>
+//       {children}
+//     </div>
+//   );
+// }
 "use client";
 
-// src/features/customer/components/CustomerCard.jsx
-import React from "react";
-
 export function CustomerCard({ icon, title, children }) {
-  const Icon = icon;
-
   return (
     <div className="service-card">
-      <Icon className="icon" />
+      <div className="icon">{icon}</div>
       <h3>{title}</h3>
       {children}
     </div>
