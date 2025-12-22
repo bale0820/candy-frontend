5bb7d7ecd0b3b0bdb2336bd8f2b55277c4aab0b2
unknown <dbsgywls667@gmail.com>
2025-12-22 23:35:46 +0900

    IMAGE_BASE_URL등 env.local 로변경

--- shared/constants/clientEnv.js
+++ shared/constants/clientEnv.js
@@ -1,3 +1,4 @@
 export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 export const IMAGE_BASE_URL = process.env.NEXT_PUBLIC_IMAGE_BASE_URL;
-export const AUTH_BASE_URL=process.env.NEXT_PUBLIC_AUTH_BASE_URL
+export const AUTH_BASE_URL=process.env.NEXT_PUBLIC_AUTH_BASE_URL;
+export const PRODUCT_API_BASE_URL=process.env.NEXT_PUBLIC_PRODUCT_API_BASE_URL;
