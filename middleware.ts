e40cbef08fe76c56cecaafbe540dbc26f806c3b5
shyun <exriik@gmail.com>
2026-01-16 10:45:28 +0900

    middleware.ts 추가

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  if (pathname.startsWith("/admin")) {
+    const role = request.cookies.get("role")?.value;
+    const accessToken = request.cookies.get("accessToken")?.value;
+
+    if (!accessToken || role !== "ADMIN") {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/admin/:path*"],
+};
