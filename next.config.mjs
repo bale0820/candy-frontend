00ea3d6136d0fbd7a9f1f6b56e334e6fc682b624
unknown <dbsgywls667@gmail.com>
2026-05-14 00:12:38 +0900

    fix: 첫 커밋\

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,12 @@
 const nextConfig = {
   reactCompiler: false,
 
-  compiler: {
-    removeConsole:
-      process.env.NODE_ENV === "production"
-        ? { exclude: ["error"] }
-        : false,
-  },
+  // compiler: {
+  //   removeConsole:
+  //     process.env.NODE_ENV === "production"
+  //       ? { exclude: ["error"] }
+  //       : false,
+  // },
 };
 
 export default nextConfig;
